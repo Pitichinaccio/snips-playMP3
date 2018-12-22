0d96cbbe198457f224722675a8cb29cf62c9a8b5
Pitichinaccio <43654725+Pitichinaccio@users.noreply.github.com>
2018-12-23 00:42:02 +0100

    Update action-playMP3.py

--- action-playMP3.py
+++ action-playMP3.py
@@ -25,7 +25,7 @@ CONFIGURATION_ENCODING_FORMAT = "utf-8"
 #     except (IOError, ConfigParser.Error) as e:
 #         return dict()
 def play_mp3(path):
-    subprocess.Popen(['mpg123', '-q', path]).wait()
+    subprocess.Popen(['mpg321', '-q', path]).wait()
 
 def subscribe_intent_callback(hermes, intentMessage):
     # conf = read_configuration_file(CONFIG_INI)
